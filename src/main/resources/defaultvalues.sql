292a83bc0d1c40283e70c2dc388fbe14797fc011
Randeep <randeepsp@outlook.com>
2019-06-08 20:01:50 +0530

    Committing missing resources files

---
+++ src/main/resources/defaultvalues.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO MoneyUser (PhoneNumber,UserName, EmailId) VALUES ('1234','Randeep','randeep@gmail.com');
+INSERT INTO MoneyUser (PhoneNumber,UserName, EmailId) VALUES ('5678','Tom','tom@gmail.com');
+INSERT INTO MoneyUser (PhoneNumber,UserName, EmailId) VALUES ('9012','LingWang','lingwang@gmail.com');
+INSERT INTO MoneyUser (PhoneNumber,UserName, EmailId) VALUES ('9789','Zoltar','zoltar@gmail.com');
+INSERT INTO MoneyUser (PhoneNumber,UserName, EmailId) VALUES ('12345','DeleteUser','deleteuser@gmail.com');
+INSERT INTO MoneyUser (PhoneNumber,UserName, EmailId) VALUES ('567890','Ali','ali@gmail.com');
+INSERT INTO MoneyUser (PhoneNumber,UserName, EmailId) VALUES ('1111','from','from@gmail.com');
+INSERT INTO MoneyUser (PhoneNumber,UserName, EmailId) VALUES ('2222','to','to@gmail.com');
+
+INSERT INTO MoneyAccount (PhoneNumber,Balance,Currency) VALUES ('1234',100.0000,'USD');
+INSERT INTO MoneyAccount (PhoneNumber,Balance,Currency) VALUES ('5678',200.0000,'USD');
+INSERT INTO MoneyAccount (PhoneNumber,Balance,Currency) VALUES ('9012',500.0000,'EUR');
+INSERT INTO MoneyAccount (PhoneNumber,Balance,Currency) VALUES ('567890',180.0000,'USD');
+INSERT INTO MoneyAccount (PhoneNumber,Balance,Currency) VALUES ('1111',100.0000,'USD');
+INSERT INTO MoneyAccount (PhoneNumber,Balance,Currency) VALUES ('2222',110.0000,'USD');
